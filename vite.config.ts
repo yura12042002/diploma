e5dad95357638bc5b772aa4b7d4c17a1e5ec47c1
yura12042002 <your_email@example.com>
2025-03-13 04:56:07 +0500

    Initial comm 12312it

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Diploma-shfe/',
   plugins: [react()],
-})
+  base: "/diploma/", // 👈 Правильный base для GitHub Pages
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+});
